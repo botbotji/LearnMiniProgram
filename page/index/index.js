78e54ba661fab42034376941615476c67913c34b
botbotji <1840226953@qq.com>
2020-05-20 13:10:51 +0800

    网络请求API及其封装

--- page/index/index.js
+++ page/index/index.js
@@ -1,6 +1,8 @@
 // page/index/index.js
+//导入封装的方法
+import request from '../../service/network.js'
 Page({
-
+  
   /**
    * 页面的初始数据
    */
@@ -12,7 +14,44 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    //封装的方法
+    request({
+        //使用封装的request
+        //Promise最大的好处就是防止出现回调地狱
+              url:"http://httpbin.org/post",
+              method:"post"
+            }).then((res) => {
+              //成功回调
+              console.log(res)
+            }).catch(err => {
+              //失败回调
+              console.log(err)
+          })
+      //发送网络请求
+      // wx.request({
+      //   //默认get请求方式
+      //   //http://httpbin.org 测试url的开放地址
+      //   url: 'http://httpbin.org/post',
+      //   success:function(res){
+      //     //成功
+      //     console.log("--------",res)
+      //   },
+      //   fail:function(){
+      //     //失败
+      //   },
+      //   data:{
+      //     //url上的参数
+      //     name: 'codename',
+      //     age: 18
+      //   },
+      //   method: 'post',
+      //   header:{
+      //     'context-type':'application/json'
+      //   },
+      //   dataType:{
+      //     'context-type':'application/json'
+      //   },
+      // })
   },
 
   /**
